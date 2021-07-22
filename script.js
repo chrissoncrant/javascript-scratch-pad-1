7364d66450cd2e19120b3a3e6e360d0f937ae9e4
Chris Soncrant <christopher.soncrant@gmail.com>
2021-07-21 19:04:04 -0700

    added more practice scripts for various exercises.

--- script.js
+++ script.js
@@ -1,8 +1,177 @@
-function getLastLetter(string) {
-    return string.slice((string.length - 1), (string.length));
+
+//===================================================
+//===================================================
+//===================================================
+//TRANSFORMING CASE
+
+let str = "camelCaseYourAss";
+
+
+function lowerCase(str){
+    str = str.replace(/([A-Z])/g, " $1");
+    str = str.replace(/([A-Z])/g, u => u.toLowerCase());
+    console.log(str);
+    return str.replace(/^[a-z]/, u => u.toUpperCase());
+  }
+
+console.log(lowerCase(str));
+
+
+
+//===================================================
+//===================================================
+//===================================================
+//INTERSECTION FINDER
+
+/*
+let strArr = ["1, 2, 4, 5, 6, 11, 13", "3, 5, 11, 25, 4"];
+
+function findIntersection(strArr) {
+    let str1Arr = strArr[0].split(", ");
+
+    let str2Arr = strArr[1].split(", ");
+    
+    let intArr = [];
+
+    for (let i = 0; i < str1Arr.length; i++) {
+        for (let j = 0; j < str2Arr.length; j++) {
+            if (str1Arr[i] === str2Arr[j]) {
+                intArr.push(str1Arr[i]);
+            }
+        }
+    }
+    return intArr.join(", ");
 }
 
-console.log(getLastLetter("chicken"));
+console.log(findIntersection(strArr));
+*/
+
+
+//===================================================
+//===================================================
+//===================================================
+
+// Find Longest String:
+/* Version 2:
+let sen = "I'm the&%% strings89743(*(*())) thing''s'ss"; //The Input
+
+sen = sen.replace(/[^A-Za-z0-9 ]/g, "");
+
+let longest;
+
+let arr = sen.split(" ");
+
+console.log(arr);
+
+arr = arr.sort(function(a, b) {
+    return b.length - a.length
+});
+
+console.log(arr);
+
+longest = arr[0];
+
+console.log(longest);
+*/
+
+
+/* Version 1:
+function findLargestStr(sen) {
+    function splitStrIntoArray(sen) {
+        let arr = sen.split(" ");
+        return arr;
+    }
+
+    let strArray = splitStrIntoArray(sen);
+
+    let largestStr = strArray[0];
+
+    function calcTrueLength(str1) {
+        let length1 = str1.length;
+        let length2 = 0;
+        if (str1.match(/[^A-Za-z0-9]/g)) {
+            let length2 = str1.match(/[^A-Za-z0-9]/g).length;
+            return length1 - length2;  
+        } else {return length1};  
+    }
+
+    let largestStrLength = calcTrueLength(largestStr);
+    
+    for (let i = 1; i < strArray.length; i++) {
+        let a = calcTrueLength(strArray[i]);
+        if (a > largestStrLength) {
+            largestStr = strArray[i];
+            largestStrLength = a;
+        }
+    }
+    return largestStr;
+}
+
+console.log(findLargestStr(sen))
+*/
+
+
+//===================================================
+//===================================================
+//===================================================
+
+//STRING VALIDATION
+/*
+function usernameValidation(str) { 
+
+    //console.log(str.slice(0, 1) === regex)
+
+    /*
+    if (str.length < 4 || str.length > 25) {
+      return "invalid";
+    } 
+
+    if (str.slice(0,1).match(/[^A-Za-z]/)) {
+        console.log("Enter a number for the first letter.")
+    };
+    
+    if (str.slice(str.length - 1, str.length).match(/[^_]/)) {
+        console.log("Not an underscore");
+    } else {
+        console.log("You can't use an underscore for the last character!")
+    };
+
+    if (str.slice(1, str.length).match(/[A-Za-z0-9_]/)) {
+        console.log("body characters are legit");
+    }
+    */
+/*
+    const strLength = str.length;
+
+    if (
+        (strLength > 4 && strLength < 25) && 
+        (str.slice(0, 1).match(/[^0-9_]/)) && 
+        (str.slice(1, strLength).match(/[A-za-z0-9_]/)) && 
+        (str.slice(strLength - 1, strLength).match(/[^_]/))
+        ) {
+            return "Valid";
+        } else {
+            return "Invalid";
+        }
+    }
+
+
+console.log(usernameValidation("Hills"));
+*/
+
+/* STRING REVERSAL
+
+function firstReverse(str) {
+    let arr = [];
+    for (let i = str.length; i > 0; i--) {
+        arr.push(str.slice((i - 1), i));
+    }
+    str = arr.join("");
+    return str;
+}
+
+console.log(firstReverse("eggs and bacon"))
+*/
 
 //==========================================
 
@@ -56,4 +225,3 @@ if (answer === "ECMAScript") {
     alert("You don't know? \"ECMASript\"");
 }
 */
-
